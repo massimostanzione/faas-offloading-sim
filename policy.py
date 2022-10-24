ec84c448c8230d82e0a2935f355150a11561d8af
Gabriele Russo Russo <gabri.russo17@gmail.com>
2022-10-24 15:08:24 +0200

    EMA for arrival rate estimation

--- policy.py
+++ policy.py
@@ -48,10 +48,14 @@ class ProbabilisticPolicy (Policy):
 
     def __init__ (self, simulation):
         super().__init__(simulation)
-        self.probs = {(f,c): [0.33,0.33,1-0.66] for f in simulation.functions for c in simulation.classes}
         seed = self.simulation.config.getint(conf.SEC_POLICY,"seed", fallback=13)
         self.rng = np.random.default_rng(seed)
         self.stats_snapshot = None
+        self.last_update_time = None
+        self.arrival_rate_alpha = self.simulation.config.getfloat(conf.SEC_POLICY, conf.POLICY_ARRIVAL_RATE_ALPHA, fallback=1.0)
+        self.arrival_rates = {}
+
+        self.probs = {(f,c): [0.33,0.33,1-0.66] for f in simulation.functions for c in simulation.classes}
 
     def schedule (self, f, c):
         probabilities = self.probs[(f,c)]
@@ -67,27 +71,33 @@ class ProbabilisticPolicy (Policy):
         return decision
 
     def update(self):
-        if self.stats_snapshot is not None:
-            stats = self.simulation.stats
+        estimated_service_time = {}
+        estimated_service_time_cloud = {}
+        for f in self.simulation.functions:
+            # TODO: use an estimate instead of the true value
+            estimated_service_time[f] = f.serviceMean/self.simulation.edge.speedup
+            estimated_service_time_cloud[f] = f.serviceMean/self.simulation.cloud.speedup
 
+        stats = self.simulation.stats
+        if self.stats_snapshot is not None:
             arrival_rates = {}
             for f,c in stats.arrivals:
-                arrival_rates[(f,c)] = stats.arrivals[(f,c)]/self.simulation.t
+                new_arrivals = stats.arrivals[(f,c)] - self.stats_snapshot["arrivals"][repr((f,c))] 
+                new_rate = new_arrivals/(self.simulation.t - self.last_update_time)
+                self.arrival_rates[(f,c)] = self.arrival_rate_alpha*new_rate +\
+                   (1.0-self.arrival_rate_alpha)* self.arrival_rates[(f,c)]
+        else:
+            for f,c in stats.arrivals:
+                self.arrival_rates[(f,c)] = stats.arrivals[(f,c)]/self.simulation.t
 
-            estimated_service_time = {}
-            estimated_service_time_cloud = {}
-            for f in self.simulation.functions:
-                # TODO: use an estimate instead of the true value
-                estimated_service_time[f] = f.serviceMean/self.simulation.edge.speedup
-                estimated_service_time_cloud[f] = f.serviceMean/self.simulation.cloud.speedup
-
-            self.probs = optimizer.update_probabilities(self.simulation,
-                                arrival_rates,
-                                estimated_service_time,
-                                estimated_service_time_cloud,
-                                self.simulation.init_time[self.simulation.edge],
-                                2*self.simulation.latencies[(self.simulation.edge.region,self.simulation.cloud.region)])
+        self.probs = optimizer.update_probabilities(self.simulation,
+                            self.arrival_rates,
+                            estimated_service_time,
+                            estimated_service_time_cloud,
+                            self.simulation.init_time[self.simulation.edge],
+                            2*self.simulation.latencies[(self.simulation.edge.region,self.simulation.cloud.region)])
         self.stats_snapshot = self.simulation.stats.to_dict()
+        self.last_update_time = self.simulation.t
 
 class RandomPolicy (ProbabilisticPolicy):
 
