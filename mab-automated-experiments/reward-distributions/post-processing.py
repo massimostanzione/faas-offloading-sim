b381c835e520011707ed76876648c7a76cf600c4
massimostanzione <massimo.stanzione@hotmail.it>
2024-12-10 04:13:04 +0100

    [reward-distributions] minor fixes, better output

--- mab-automated-experiments/reward-distributions/post-processing.py
+++ mab-automated-experiments/reward-distributions/post-processing.py
@@ -15,11 +15,10 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 from _internal import consts
 from _internal.experiment import generate_outfile_name
-from _internal.experiment import write_custom_configfile
-from main import main
+from _internal.graphs import line_graph
 
 to_be_executed = []
-EXPNAME = "policies-choices"
+EXPNAME = "reward-distributions"
 EXPCONF_PATH = os.path.join(EXPNAME, consts.EXPCONF_FILE)
 
 def mainn():
@@ -59,46 +58,53 @@ def mainn():
 
         data = sorted(data, key=custom_sort_key)
 
-        for d in data:
-            #for strat in strategies:
-                #for ax_pre in axis_pre:
-                    #for ax_post in axis_post:
-                        #if d["strategy"] == strat and d["axis_pre"] == ax_pre and d["axis_post"] == ax_post:
-            strat=d["strategy"]
-            ax_pre=d["axis_pre"]
-            ax_post=d["axis_post"]
-            seed=d["seed"]
-            print(strat, ax_pre, ax_post, seed)
-            params=d["parameters"]
-            mabfile = generate_outfile_name(
-                consts.PREFIX_MABSTATSFILE, strat, ax_pre, ax_post,
-                list(params.keys()),
-                list(params.values()), seed
-            ) + consts.SUFFIX_MABSTATSFILE
-            with open(mabfile, 'r') as f:
-                data = json.load(f)
-            pol_fr_prev = {"random-lb": 0, "round-robin-lb": 0, "mama-lb": 0, "const-hash-lb": 0,
-                           "wrr-speedup-lb": 0,
-                           "wrr-memory-lb": 0, "wrr-cost-lb": 0}
-            time_frames=[]
-            policies=[]
-            rewards=[]
-            for dd in data:
-                #print(d)
-                # todo pre/post if d['time'] < 9000:
-                pol_fr_prev[dd['policy']] += 1
-                time_frames.append(dd['time'])
-                policies.append(dd['policy'])
-                rewards.append(dd['reward'])
-            #fig =  plot_rewards(time_frames, rewards, policies, strat+" seed="+seed+" "+ax_pre+" -> "+ax_post)
-            fig = line_graph(glob_var_coeff, title="rewards " + strat + " " + ax_pre)
+        result_dict = {}
+        for strat in strategies:
+            for ax_pre in axis_pre:
+                for ax_post in axis_post:
 
-            graphs_ctr += 1
-            fig.savefig(os.path.join(SCRIPT_DIR, "results",
-                                     consts.DELIMITER_HYPHEN.join(
-                                         [str(timestamp), str(graphs_ctr)]).replace(' ',
-                                                                                    '-') + ".svg"))
-            fig.clf()
+                    for seed in seeds:
+                        time_frames = []
+                        rewards = []
+
+                        for d in data:
+                            if (
+                                    d["seed"] == seed
+                                    and d["strategy"] == strat
+                                    and d["axis_pre"] == ax_pre
+                                    and d["axis_post"] == ax_post
+                            ):
+                                params = d["parameters"]
+                                mabfile = generate_outfile_name(
+                                    consts.PREFIX_MABSTATSFILE,
+                                    strat,
+                                    ax_pre,
+                                    ax_post,
+                                    list(params.keys()),
+                                    list(params.values()),
+                                    seed,
+                                ) + consts.SUFFIX_MABSTATSFILE
+
+                                with open(mabfile, "r") as f:
+                                    print(mabfile)
+                                    data_file = json.load(f)
+
+                                # Raccogli time_frames e rewards
+                                for dd in data_file:
+                                    time_frames.append(dd["time"])
+                                    rewards.append(dd["reward"])
+                        #todo va aggiunto l'istante di cambio asse (accorpare in grafico)
+                        result_dict[seed] = {"x": time_frames, "y": rewards}
+                        print(result_dict)
+
+                    fig = line_graph(result_dict, title="rewards " + strat + " " + ax_pre+" -> "+ax_post)
+
+                    graphs_ctr += 1
+                    fig.savefig(os.path.join(SCRIPT_DIR, "results",
+                                             consts.DELIMITER_HYPHEN.join(
+                                                 [str(timestamp), str(graphs_ctr)]).replace(' ',
+                                                                                            '-') + ".svg"))
+                    fig.clf()
 
 
 
