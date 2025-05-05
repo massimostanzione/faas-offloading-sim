0c1ea53d1e0200f6f225350e934fd63fa071a942
massimostanzione <massimo.stanzione@hotmail.it>
2025-05-05 19:11:03 +0200

    close https://github.com/massimostanzione/faas-offloading-sim/issues/9

--- mab-automated-experiments/run.py
+++ mab-automated-experiments/run.py
@@ -14,19 +14,35 @@ if __name__ == "__main__":
                 with open(experiment_confpath, "r") as expconf_file:
                     config = conf.parse_config_file(experiment_confpath)
 
-                    mode_simulations = config["experiment"]["mode-simulations"]
+                    mode_preprocessing = config["experiment"]["mode-preprocessing"]
+                    mode_simulations_expconf = config["experiment"]["mode-simulations"]
                     mode_postprocessing = config["experiment"]["mode-postprocessing"]
 
                     # =========================================================
-                    # 1. Simulations
+                    # 1. Pre-processing
                     # ---------------------------------------------------------
 
-                    if mode_simulations == consts.ExecMode.NONE.value:
-                        print("Simulations skipped")
+                    if mode_preprocessing == consts.ExecMode.NONE.value:
+                        print("Preprocessing skipped")
 
-                    elif mode_simulations == consts.ExecMode.AUTOMATED.value:
-                        axis_pre = config["reward_fn"]["axis_pre"].split(consts.DELIMITER_COMMA)
-                        axis_post = config["reward_fn"]["axis_post"].split(consts.DELIMITER_COMMA)
+                    elif mode_preprocessing == consts.ExecMode.AUTOMATED.value:
+                        print("No automated preprocessing defined")
+
+                    else:
+                        name = config["experiment"]["name"]
+                        os.system("python3 " + os.path.join(name, mode_preprocessing))
+
+                    # =========================================================
+                    # 2. Simulations
+                    # ---------------------------------------------------------
+
+                    if mode_simulations_expconf == consts.ExecMode.NONE.value:
+                        print(f"Simulations skipped (as specified in {consts.EXPCONF_FILE})")
+
+                    elif mode_simulations_expconf == consts.ExecMode.AUTOMATED.value:
+                        # TODO questa parte è in api
+                        axis_pre = config["reward_fn"]["axis_pre"].replace(' ', '').split(consts.DELIMITER_COMMA)
+                        axis_post = config["reward_fn"]["axis_post"].replace(' ', '').split(consts.DELIMITER_COMMA)
                         is_single_axis = axis_post == ['']
                         exp = MABExperiment(
                             config,
@@ -35,7 +51,7 @@ if __name__ == "__main__":
                             config["experiment"]["close-door-time"],
                             config["experiment"]["mab-update-interval"],
                             axis_pre,
-                            axis_post if not is_single_axis else axis_pre,
+                            axis_post, # if not is_single_axis else axis_pre,
                             extract_iterable_params_from_config(config),
                             [],
                             config["output"]["run-duplicates"],
@@ -53,10 +69,10 @@ if __name__ == "__main__":
                         print(f"Running experiment \"{name}\"")
                         print(f"with custom simulations")
                         print("--------------------------------------------")
-                        os.system("python3 " + os.path.join(name, mode_simulations))
+                        os.system("python3 " + os.path.join(name, mode_simulations_expconf))
 
                     # =========================================================
-                    # 2. Post-processing
+                    # 3. Post-processing
                     # ---------------------------------------------------------
 
                     if mode_postprocessing == consts.ExecMode.NONE.value:
